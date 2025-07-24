adedec3dd81ff0dc49e934a6d0f058ab3e5f4404
Logan Nelson <Lnelson0915@gmail.com>
2025-07-23 18:18:25 -0700

    Adding Form Load

--- MinecraftBlockIndex/Form1.Designer.cs
+++ MinecraftBlockIndex/Form1.Designer.cs
@@ -37,6 +37,7 @@
             ClientSize = new Size(840, 511);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
