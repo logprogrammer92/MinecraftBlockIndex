2b20354bb68a04ab7d0807cdda25393d3a88ceb1
Toasty <toastedtoast0090@gmail.com>
2025-08-02 00:31:27 -0700

    Added clarifying comment in NeedHelp and added blank space for readability

--- MinecraftBlockIndex/Block_additon_handling/addBlockForm.cs
+++ MinecraftBlockIndex/Block_additon_handling/addBlockForm.cs
@@ -29,12 +29,14 @@ namespace MinecraftBlockIndex.Block_additon_handling
 
         private void btnNeedHelp_Click(object sender, EventArgs e)
         {
+            //This help message explains to the user what each check is for.
             string helpMessage = 
                 "Block Name: The unique name for the block/n" + 
                 "Emits Light: Does the block emit light when left alone/n" +
                 "Burnable: Can the block be burned by fire and lava?/n" +
                 "Full Block: Is the block a full block or does it have space missing like a slab or stairs/n" +
                 "Transparent: Can you see through the block like glass or leaves (depending on version)";
+
             MessageBox.Show(helpMessage);
         }
     }
