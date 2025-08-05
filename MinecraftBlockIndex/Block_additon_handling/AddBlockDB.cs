687ef5b5fd778f4e8ff4989c94d1da50b0a685e7
Toasty <toastedtoast0090@gmail.com>
2025-08-04 21:35:54 -0700

    Update connection string in AddBlockDB class
    Modified the connection string in the `GetDatabaseConnection` method to use "localdb" instead of "localhost". This change facilitates the use of a local database instance for development or testing purposes.

--- MinecraftBlockIndex/Block_additon_handling/AddBlockDB.cs
+++ MinecraftBlockIndex/Block_additon_handling/AddBlockDB.cs
@@ -13,7 +13,7 @@ namespace MinecraftBlockIndex.Block_additon_handling
         private static SqlConnection GetDatabaseConnection()
         {
             // Establish a connection to the database
-            return new SqlConnection("Data Source=localhost;Initial Catalog=MinecraftBlockIndex;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            return new SqlConnection("Data Source=localdb;Initial Catalog=MinecraftBlockIndex;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
         }
     }
