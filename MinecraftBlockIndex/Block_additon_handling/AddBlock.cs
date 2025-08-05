5ce7561b4647dcbad48f440f98cdb22929d77247
Toasty <toastedtoast0090@gmail.com>
2025-08-05 13:04:49 -0700

    Refactor AddBlock properties and add constructor
    Changed BlockID to have a private setter and made BlockName a required property. Added a constructor for initializing all properties with validation for BlockName to ensure it is not null or empty.

--- MinecraftBlockIndex/Block_additon_handling/AddBlock.cs
+++ MinecraftBlockIndex/Block_additon_handling/AddBlock.cs
@@ -8,12 +8,25 @@ namespace MinecraftBlockIndex.Block_additon_handling
 {
     internal class AddBlock
     {
-        public int BlockID { get; set; } //Unique ID for block
-        public string BlockName { get; set; } //Name of block tied to ID
+        public int BlockID { get; private set; } //Unique ID for block
+        public required string BlockName { get; set; } //Name of block tied to ID
         public bool IsBurnable { get; set; } //Indicates if block can be burned by fire or lava
         public bool IsTransparent { get; set; } //Indicates if block is transparent ie glass, leaves
         public bool IsFull { get; set; } //Indicates if solid block ie dirt, stone as opposed to slabs or stairs (not full blocks)
         public bool EmitsLight { get; set; } //Indicates if block emits light (ie glowstone, sea lanterns)
+        
+        // Constructor with verification and automatic ID assignment
+        public AddBlock(int blockID, string blockName, bool isBurnable, bool isTransparent, bool isFull, bool emitsLight)
+        {
+            if (string.IsNullOrWhiteSpace(blockName))
+                throw new ArgumentException("Block name cannot be null or empty.", nameof(blockName));
 
+            BlockID = blockID;
+            BlockName = blockName;
+            IsBurnable = isBurnable;
+            IsTransparent = isTransparent;
+            IsFull = isFull;
+            EmitsLight = emitsLight;
+        }
     }
 }
