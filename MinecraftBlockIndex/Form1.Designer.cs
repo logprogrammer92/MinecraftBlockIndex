90cee007a696de4351f8ebdd6f4f1b5371c8c5ea
Toasty <toastedtoast0090@gmail.com>
2025-08-11 23:23:06 -0700

    Add UI components and functionality for block management
    Implemented buttons for adding, removing, and viewing blocks, along with a help button and a combo box for block selection. Updated the form title to "Minecraft Block Index" and adjusted the client size. Added event handlers for exit and help buttons, providing application closure and user guidance through a detailed help message.

--- MinecraftBlockIndex/Form1.Designer.cs
+++ MinecraftBlockIndex/Form1.Designer.cs
@@ -28,18 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnAddBlock = new Button();
+            btnExit = new Button();
+            btnRemoveBlock = new Button();
+            btnHelp = new Button();
+            comboBoxBlockName = new ComboBox();
+            btnViewBlock = new Button();
+            groupBoxBlockView = new GroupBox();
+            lblBlockName = new Label();
+            groupBoxBlockView.SuspendLayout();
             SuspendLayout();
+            // 
+            // btnAddBlock
+            // 
+            btnAddBlock.Location = new Point(12, 115);
+            btnAddBlock.Name = "btnAddBlock";
+            btnAddBlock.Size = new Size(160, 71);
+            btnAddBlock.TabIndex = 0;
+            btnAddBlock.Text = "Add A Block";
+            btnAddBlock.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(362, 153);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(132, 33);
+            btnExit.TabIndex = 1;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnRemoveBlock
+            // 
+            btnRemoveBlock.Location = new Point(178, 115);
+            btnRemoveBlock.Name = "btnRemoveBlock";
+            btnRemoveBlock.Size = new Size(178, 71);
+            btnRemoveBlock.TabIndex = 2;
+            btnRemoveBlock.Text = "Remove A Block";
+            btnRemoveBlock.UseVisualStyleBackColor = true;
+            // 
+            // btnHelp
+            // 
+            btnHelp.Location = new Point(362, 115);
+            btnHelp.Name = "btnHelp";
+            btnHelp.Size = new Size(132, 32);
+            btnHelp.TabIndex = 3;
+            btnHelp.Text = "Guide";
+            btnHelp.UseVisualStyleBackColor = true;
+            btnHelp.Click += btnHelp_Click;
+            // 
+            // comboBoxBlockName
+            // 
+            comboBoxBlockName.FormattingEnabled = true;
+            comboBoxBlockName.Location = new Point(166, 15);
+            comboBoxBlockName.Name = "comboBoxBlockName";
+            comboBoxBlockName.Size = new Size(146, 23);
+            comboBoxBlockName.TabIndex = 4;
+            // 
+            // btnViewBlock
+            // 
+            btnViewBlock.Location = new Point(166, 59);
+            btnViewBlock.Name = "btnViewBlock";
+            btnViewBlock.Size = new Size(126, 23);
+            btnViewBlock.TabIndex = 5;
+            btnViewBlock.Text = "View Block";
+            btnViewBlock.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxBlockView
+            // 
+            groupBoxBlockView.Controls.Add(lblBlockName);
+            groupBoxBlockView.Controls.Add(comboBoxBlockName);
+            groupBoxBlockView.Controls.Add(btnViewBlock);
+            groupBoxBlockView.Location = new Point(12, 21);
+            groupBoxBlockView.Name = "groupBoxBlockView";
+            groupBoxBlockView.Size = new Size(482, 88);
+            groupBoxBlockView.TabIndex = 6;
+            groupBoxBlockView.TabStop = false;
+            groupBoxBlockView.Text = "Minecraft Block Info";
+            // 
+            // lblBlockName
+            // 
+            lblBlockName.AutoSize = true;
+            lblBlockName.Location = new Point(89, 19);
+            lblBlockName.Name = "lblBlockName";
+            lblBlockName.Size = new Size(71, 15);
+            lblBlockName.TabIndex = 6;
+            lblBlockName.Text = "Block Name";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(840, 511);
+            ClientSize = new Size(506, 208);
+            Controls.Add(groupBoxBlockView);
+            Controls.Add(btnHelp);
+            Controls.Add(btnRemoveBlock);
+            Controls.Add(btnExit);
+            Controls.Add(btnAddBlock);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Minecraft Block Index";
+            groupBoxBlockView.ResumeLayout(false);
+            groupBoxBlockView.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnAddBlock;
+        private Button btnExit;
+        private Button btnRemoveBlock;
+        private Button btnHelp;
+        private ComboBox comboBoxBlockName;
+        private Button btnViewBlock;
+        private GroupBox groupBoxBlockView;
+        private Label lblBlockName;
     }
 }
--- MinecraftBlockIndex/Form1.cs
+++ MinecraftBlockIndex/Form1.cs
@@ -6,5 +6,25 @@ namespace MinecraftBlockIndex
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            string helpMessage = "This application lets you view various blocks in the video game Minecraft\n" +
+                                    "Minecraft has a lot of blocks and so this app is meant to store block data\n" +
+                                    "For further reference.\n\n" +
+
+                                    "In the upper half you can view each item name and upon clicking 'View Block'\n" +
+                                    "you will be taken to a detailed view of the block's information\n\n" +
+
+                                    "In the lower half you will find various buttons to either add a block to the\n" +
+                                    "database for future viewing, remove a block from the database, or bring up \n" +
+                                    "this handy guide again!";
+            MessageBox.Show(helpMessage);
+        }
     }
 }
