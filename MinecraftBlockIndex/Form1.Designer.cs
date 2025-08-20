5651be4d12698d570f6f0b31e2f0b17059939c76
Toasty <toastedtoast0090@gmail.com>
2025-08-19 23:03:28 -0700

    Changed the layout of Form1 to be more usable

--- MinecraftBlockIndex/Form1.Designer.cs
+++ MinecraftBlockIndex/Form1.Designer.cs
@@ -41,9 +41,9 @@
             // 
             // btnAddBlock
             // 
-            btnAddBlock.Location = new Point(12, 115);
+            btnAddBlock.Location = new Point(12, 97);
             btnAddBlock.Name = "btnAddBlock";
-            btnAddBlock.Size = new Size(160, 71);
+            btnAddBlock.Size = new Size(160, 32);
             btnAddBlock.TabIndex = 0;
             btnAddBlock.Text = "Add A Block";
             btnAddBlock.UseVisualStyleBackColor = true;
@@ -51,9 +51,9 @@
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(362, 153);
+            btnExit.Location = new Point(178, 134);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(132, 33);
+            btnExit.Size = new Size(178, 33);
             btnExit.TabIndex = 1;
             btnExit.Text = "Exit";
             btnExit.UseVisualStyleBackColor = true;
@@ -61,18 +61,18 @@
             // 
             // btnRemoveBlock
             // 
-            btnRemoveBlock.Location = new Point(178, 115);
+            btnRemoveBlock.Location = new Point(178, 97);
             btnRemoveBlock.Name = "btnRemoveBlock";
-            btnRemoveBlock.Size = new Size(178, 71);
+            btnRemoveBlock.Size = new Size(178, 33);
             btnRemoveBlock.TabIndex = 2;
             btnRemoveBlock.Text = "Remove A Block";
             btnRemoveBlock.UseVisualStyleBackColor = true;
             // 
             // btnHelp
             // 
-            btnHelp.Location = new Point(362, 115);
+            btnHelp.Location = new Point(12, 135);
             btnHelp.Name = "btnHelp";
-            btnHelp.Size = new Size(132, 32);
+            btnHelp.Size = new Size(160, 32);
             btnHelp.TabIndex = 3;
             btnHelp.Text = "Guide";
             btnHelp.UseVisualStyleBackColor = true;
@@ -82,18 +82,18 @@
             // 
             comboBoxBlock.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBoxBlock.FormattingEnabled = true;
-            comboBoxBlock.Location = new Point(166, 15);
+            comboBoxBlock.Location = new Point(83, 16);
             comboBoxBlock.Name = "comboBoxBlock";
-            comboBoxBlock.Size = new Size(146, 23);
+            comboBoxBlock.Size = new Size(255, 23);
             comboBoxBlock.TabIndex = 4;
             comboBoxBlock.SelectedIndexChanged += comboBoxBlock_SelectedIndexChanged;
             // 
             // btnViewBlock
             // 
             btnViewBlock.Enabled = false;
-            btnViewBlock.Location = new Point(166, 59);
+            btnViewBlock.Location = new Point(6, 47);
             btnViewBlock.Name = "btnViewBlock";
-            btnViewBlock.Size = new Size(126, 23);
+            btnViewBlock.Size = new Size(332, 23);
             btnViewBlock.TabIndex = 5;
             btnViewBlock.Text = "View Block";
             btnViewBlock.UseVisualStyleBackColor = true;
@@ -104,9 +104,9 @@
             groupBoxBlockView.Controls.Add(lblBlockName);
             groupBoxBlockView.Controls.Add(comboBoxBlock);
             groupBoxBlockView.Controls.Add(btnViewBlock);
-            groupBoxBlockView.Location = new Point(12, 21);
+            groupBoxBlockView.Location = new Point(12, 12);
             groupBoxBlockView.Name = "groupBoxBlockView";
-            groupBoxBlockView.Size = new Size(482, 88);
+            groupBoxBlockView.Size = new Size(344, 79);
             groupBoxBlockView.TabIndex = 6;
             groupBoxBlockView.TabStop = false;
             groupBoxBlockView.Text = "Minecraft Block Info";
@@ -114,7 +114,7 @@
             // lblBlockName
             // 
             lblBlockName.AutoSize = true;
-            lblBlockName.Location = new Point(89, 19);
+            lblBlockName.Location = new Point(6, 19);
             lblBlockName.Name = "lblBlockName";
             lblBlockName.Size = new Size(71, 15);
             lblBlockName.TabIndex = 6;
@@ -124,7 +124,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(506, 319);
+            ClientSize = new Size(368, 175);
             Controls.Add(groupBoxBlockView);
             Controls.Add(btnHelp);
             Controls.Add(btnRemoveBlock);
